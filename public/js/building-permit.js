5b61b0b1380e9ef7d78f6fea646cee744527f627
DuarteC <DuarteC@IT-WKS001.MidNet.CityOfMiddletown.com>
2018-08-07 16:20:34 -0400

    Mask input form for formatted fields

--- public/js/building-permit.js
+++ public/js/building-permit.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 	$('#ESTIMATED_COSTS').on('input', function() {
 		$estimated_costs = accounting.unformat($(this).val());
 		$permit_fee = 15.26 + (Math.ceil($estimated_costs / 1000) - 1) * 14.26;
-		$('#PERMIT_FEE').val($permit_fee);
+		$('#PERMIT_FEE').val(accounting.formatNumber($permit_fee, 2, ",", "."));
 	});
+	
+	$('#APPLICANTS_PHONE').mask('000-000-0000');
+	$('#APPLICANTS_FAX').mask('000-000-0000');
+	$('#APPLICANTS_ZIP').mask('00000-0000');
+	$('#OWNERS_ZIP').mask('00000-0000');
 });
