2c74bc42340650dfad13acf3812e67a5b7d7ba29
DuarteC <DuarteC@IT-WKS001.MidNet.CityOfMiddletown.com>
2018-08-13 10:03:37 -0400

    Added $50 floor on estimated costs

--- public/js/building-permit.js
+++ public/js/building-permit.js
@@ -1,9 +1,14 @@
 $(document).ready(function() { 
 	$('#ESTIMATED_COSTS').on('input', function() {
 		$estimated_costs = accounting.unformat($(this).val());
-		$permit_fee = 15.26 + (Math.ceil($estimated_costs / 1000) - 1) * 14.26;
-		$('#PERMIT_FEE').val(accounting.formatNumber($permit_fee, 2, ",", "."));
-		$('#Q').val(accounting.formatNumber($permit_fee, 2, ",", "."));
+		if ($estimated_costs < 50) {
+			$('#PERMIT_FEE').val(0.00);
+			$('#Q').val(0.00);
+		} else {
+			$permit_fee = 15.26 + (Math.ceil($estimated_costs / 1000) - 1) * 14.26;
+			$('#PERMIT_FEE').val(accounting.formatNumber($permit_fee, 2, ",", "."));
+			$('#Q').val(accounting.formatNumber($permit_fee, 2, ",", "."));
+		}
 	});
 	
 	$('#APPLICANTS_PHONE').mask('000-000-0000');
